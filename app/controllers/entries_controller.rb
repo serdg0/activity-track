8cb8e50784375f6de7643be383dd05a12c356abe
Sergio Diaz <sergio.diazg92@gmail.com>
2020-09-19 11:57:23 -0500

    signup & test user

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,10 @@
+class EntriesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   
+  get 'entries/new'
+  get 'entries/create'
+  get 'entries/index'
+  get 'activities/new'
+  get 'activities/create'
   devise_for :users
+  root to: 'entries#index'
 end
