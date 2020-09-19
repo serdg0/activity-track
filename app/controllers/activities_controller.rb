3eeb3d148e9795ed39f9ef4a84e8198f9e96ba12
Sergio Diaz <sergio.diazg92@gmail.com>
2020-09-19 12:39:54 -0500

    create activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,21 @@
 class ActivitiesController < ApplicationController
   def new
+    @activity = Activity.new
   end
 
   def create
+    @activity = current_user.activities.build(activity_params)
+    if @activity.save
+      p "Activity saved successfully!"
+      redirect_to root_path
+    else
+      p @activity.errors.messages
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:label)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  
+  resources :activities
   get 'entries/new'
   get 'entries/create'
   get 'entries/index'
-  get 'activities/new'
-  get 'activities/create'
   devise_for :users
   root to: 'entries#index'
 end
